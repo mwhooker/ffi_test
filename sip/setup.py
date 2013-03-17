41d8a45f97210cf69624fbcb527a489ad19891cd
Matthew Hooker <mwhooker@gmail.com>
2013-03-17 10:14:34 -0700

    sip working

--- sip/setup.py
+++ sip/setup.py
@@ -3,11 +3,11 @@ import sipdistutils
 
 
 setup (
-    name = 'test',
+    name = 'demo',
     version = '1.0',
     description = 'This is a demo package',
     ext_modules=[
-        Extension("test", ["test.sip", "test.c"]),
+        Extension("demo", ["demo.sip", "demo.c"]),
     ],
     cmdclass = {'build_ext': sipdistutils.build_ext}
 )
